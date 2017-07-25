d6e78cd9f404f84cc1311c5b1eb25e0bc1afe8a7
Cong MA <macong1989@gmail.com>
2017-07-25 21:15:45 +1000

    Add parsing json

--- CreateFormDemo/Controllers/HomeController.cs
+++ CreateFormDemo/Controllers/HomeController.cs
@@ -3,11 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using System.IO;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using Microsoft.AspNetCore.Hosting;
 
 namespace CreateFormDemo.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly IHostingEnvironment hostingEnvironment;
+
+        public HomeController(IHostingEnvironment hostingEnvironment)
+        {
+            this.hostingEnvironment = hostingEnvironment;
+        }
+
         public IActionResult Index()
         {
             return View();
@@ -24,6 +35,16 @@ namespace CreateFormDemo.Controllers
         {
             ViewData["Message"] = "Your contact page.";
 
+            return View();
+        }
+
+        public IActionResult Form()
+        {
+            using (StreamReader file = System.IO.File.OpenText(hostingEnvironment.ContentRootPath + "\\FormModule.json"))
+            {
+                JObject moduleForm = JObject.Parse(file.ReadToEnd());
+                string formName = moduleForm["Module"]["Name"].ToString();
+            }
             return View();
         }
 
