47f191a504c35ff98106fbe36ddac0e231cb6845
Cong MA <cong.ma@catalysthealth.com.au>
2017-07-26 16:44:42 +1000

    edit

---
+++ MvcDynamicFormsCore/Models/DynamicFormModelBinder.cs
@@ -0,0 +1,49 @@
+﻿using System.Web;
+
+namespace MvcDynamicForms.Core
+{
+    using System;
+    using System.Linq;
+    using MvcDynamicForms.Core.Fields;
+    using MvcDynamicForms.Core.Fields.Abstract;
+    using Microsoft.AspNetCore.Mvc.ModelBinding;
+    using Microsoft.AspNetCore.Mvc;
+    using System.Threading.Tasks;
+    using Microsoft.AspNetCore.Mvc.Internal;
+    using System.Text.Encodings.Web;
+
+    class DynamicFormModelBinder : IModelBinder
+    {
+        private readonly IModelBinder _fallbackBinder;
+
+        public DynamicFormModelBinder(IModelBinder fallbackBinder)
+        {
+            if (fallbackBinder == null)
+                throw new ArgumentNullException(nameof(fallbackBinder));
+
+            _fallbackBinder = fallbackBinder;
+        }
+
+        public Task BindModelAsync(ModelBindingContext bindingContext)
+        {
+            if (bindingContext == null)
+                throw new ArgumentNullException(nameof(bindingContext));
+
+            var valueProviderResult = bindingContext.ValueProvider.GetValue(bindingContext.ModelName);
+
+            if (valueProviderResult == ValueProviderResult.None)
+            {
+                var valueAsString = valueProviderResult.FirstValue;
+
+                if (string.IsNullOrEmpty(valueAsString))
+                {
+                    return _fallbackBinder.BindModelAsync(bindingContext);
+                }
+
+                var result = HtmlEncoder.Default.Encode(valueAsString);
+                bindingContext.Result = ModelBindingResult.Success(result);
+            }
+            return TaskCache.CompletedTask;
+        }
+    }
+}
---
+++ MvcDynamicFormsCore/Models/Fields/TextArea.cs
@@ -0,0 +1,53 @@
+﻿namespace MvcDynamicForms.Core.Fields
+{
+    using System;
+    using System.Text;
+    using MvcDynamicForms.Core.Fields.Abstract;
+    using Microsoft.AspNetCore.Mvc.Rendering;
+
+    /// <summary>
+    /// Represents an html textarea element.
+    /// </summary>
+    [Serializable]
+    public class TextArea : TextField
+    {
+        public override string RenderHtml()
+        {
+            var html = new StringBuilder(this.Template);
+            var inputName = this.GetHtmlId();
+
+            // prompt label
+            var prompt = new TagBuilder("label");
+            //prompt.SetInnerText(this.GetPrompt());
+            prompt.InnerHtml.Append(this.GetPrompt());
+            prompt.Attributes.Add("for", inputName);
+            prompt.Attributes.Add("class", this._promptClass);
+            html.Replace(PlaceHolders.Prompt, prompt.ToString());
+
+            // error label
+            if (!this.ErrorIsClear)
+            {
+                var error = new TagBuilder("label");
+                error.Attributes.Add("for", inputName);
+                error.Attributes.Add("class", this._errorClass);
+                //error.SetInnerText(this.Error);
+                error.InnerHtml.Append(this.Error);
+                html.Replace(PlaceHolders.Error, error.ToString());
+            }
+
+            // input element
+            var txt = new TagBuilder("textarea");
+            txt.Attributes.Add("name", inputName);
+            txt.Attributes.Add("id", inputName);
+            //txt.SetInnerText(this.Value);
+            txt.InnerHtml.Append(this.Value);
+            txt.MergeAttributes(this._inputHtmlAttributes);
+            html.Replace(PlaceHolders.Input, txt.ToString());
+
+            // wrapper id
+            html.Replace(PlaceHolders.FieldWrapperId, this.GetWrapperId());
+
+            return html.ToString();
+        }
+    }
+}
